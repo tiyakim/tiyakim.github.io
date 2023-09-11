8562d3723e7c503325d2a83cba6e84281dcab811
KennethJHan <kenneth.jh.han@gmail.com>
2023-09-11 19:55:44 +0900

    230911

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["EGFR","NSCLC","CancerResearch","PrecisionMedicine","Oncology","Pulmonology"],
         "url": "/im/lazertinib/",
         "teaser": null
+      },{
+        "title": "패혈증-sepsis",
+        "excerpt":"🧑‍⚕️ 패혈증(Sepsis) 9월 13일은 세계 패혈증의 날입니다. 패혈증(敗血症, Sepsis)은 감염과 함께 장기 부전이 있는 것으로 정의합니다 (Sepsis-3). 패혈증은 여전히 생소한 병명이라 환자분들께 설명할 때는 기억하시기 쉬우라고 패혈(敗血), 피가 부패(腐敗)하는 것, 균이 하나도 없어야 할 피 안에 균이 들어가 있어 감염이 매우 심한 것이라고 설명하곤 합니다. 패혈증에는 묶음치료 수행이 사망률을 감소시키는데...","categories": ["im"],
+        "tags": ["sepsis","패혈증","KSA","한국패혈증연대","묶음치료"],
+        "url": "/im/sepsis/",
+        "teaser": null
       }]
